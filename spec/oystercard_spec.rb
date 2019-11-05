bf9e4067d95479d5a1bd8b13e63c635088bae18d
Rahul Shah <rahulshah7@outlook.com>
2019-11-05 17:26:39 +0000

    Add initialization context

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,14 +8,18 @@ describe Oystercard do
   describe '#balance' do
     it { is_expected.to respond_to(:balance) }
 
-    subject(:card_with_zero) { described_class.new }
-    it 'returns a balance of zero' do
-      expect(card_with_zero.balance).to eq 0
+    context 'when initialized without a balance' do
+      subject(:card_with_zero) { described_class.new }
+      it 'returns a balance of zero' do
+        expect(card_with_zero.balance).to eq 0
+      end
     end
 
-    subject(:card_with_hundred) { described_class.new(balance: 100) }
-    it 'returns a balance of 100' do
-      expect(card_with_hundred.balance).to eq 100
+    context 'when initialized with a balance of 100' do
+      subject(:card_with_hundred) { described_class.new(balance: 100) }
+      it 'returns a balance of 100' do
+        expect(card_with_hundred.balance).to eq 100
+      end
     end
   end
 end
