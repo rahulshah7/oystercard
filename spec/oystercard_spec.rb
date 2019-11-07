7ba3f7ba65b049025645eec0b67473e3adfc35e8
Rahul Shah <rahulshah7@outlook.com>
2019-11-07 15:11:19 +0000

    Add tests for #touch_in / out

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -52,4 +52,21 @@ describe Oystercard do
       it { is_expected.not_to be_in_journey }
     end
   end
+
+  describe '#touch_in' do
+    it { is_expected.to respond_to(:touch_in) }
+    it 'should begin a journey' do
+      subject.touch_in
+      is_expected.to be_in_journey
+    end
+  end
+
+  describe '#touch_out' do
+    it { is_expected.to respond_to(:touch_out) }
+    it 'should end a journey' do
+      subject.touch_in
+      subject.touch_out
+      is_expected.not_to be_in_journey
+    end
+  end
 end
